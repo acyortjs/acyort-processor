042396d6c41ee6f272776d9a264fb81f5d937a2b
LoeiFy <LoeiFy@gmail.com>
2017-11-21 16:54:05 +0800

    post case test

--- test/index.js
+++ test/index.js
@@ -13,6 +13,14 @@ function rejects(promise) {
     .catch(reason => Promise.resolve(reason))
 }
 
+String.prototype.trim = function() {
+  return this
+    .replace(/\n/g, '')
+    .replace(/[\t ]+\</g, '<')
+    .replace(/\>[\t ]+\</g, '><')
+    .replace(/\>[\t ]+$/g, '>')
+}
+
 describe('processor', () => {
   it('no content', async () => {
     const msg = 'No content. Check user, repository or authors fields'
@@ -48,32 +56,33 @@ describe('processor', () => {
     const _config = JSON.parse(JSON.stringify(config))
     let processor = new Processor(_config)
     let { posts } = await processor.process(issues)
-    const post1 = {
-      id: 138194595,
-      created: '2016-03-03T14:36:26Z',
-      updated: '2017-11-03T07:10:07Z',
-      title: 'Markdown Cheatsheet',
-      path: '/post/138194595.html',
-      url: '/post/138194595.html',
-      author:
-       { name: 'LoeiFy',
-         avatar: 'https://avatars0.githubusercontent.com/u/2193211?v=4',
-         url: 'https://github.com/LoeiFy' },
-      toc: '',
-      category: { id: 2195893, name: 'test', url: 'category/2195893/' },
-      tags:
-       [ { id: 335624168, name: 'duplicate', url: 'tag/335624168/' },
-         { id: 335624169, name: 'enhancement', url: 'tag/335624169/' } ],
-      thumb: 'https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png',
-      summary: '',
-      content: '<hr>\n<p>title: LoadRuner中的参数与变量\npermalink: post/loadrunner-parameter-variable</p>\n<h2 id="tags脚本开发loadrunner">tags: [脚本开发, LoadRunner]</h2><p>Emphasis, aka italics, with <em>asterisks</em> or <em>underscores</em>.</p>\n<p>Strong emphasis, aka bold, with <strong>asterisks</strong> or <strong>underscores</strong>.</p>\n<p>Combined emphasis with <strong>asterisks and <em>underscores</em></strong>.</p>\n<p>Strikethrough uses two tildes. <del>Scratch this.</del></p>\n<ol>\n<li>First ordered list item</li><li>Another item\n⋅⋅* Unordered sub-list. </li><li>Actual numbers don&#39;t matter, just that it&#39;s a number\n⋅⋅1. Ordered sub-list</li><li>And another item.</li></ol>\n<p>⋅⋅⋅You can have properly indented paragraphs within list items. Notice the blank line above, and the leading spaces (at least one, but we&#39;ll use three here to also align the raw Markdown).</p>\n<p>⋅⋅⋅To have a line break without a paragraph, you will need to use two trailing spaces.⋅⋅\n⋅⋅⋅Note that this line is separate, but within the same paragraph.⋅⋅\n⋅⋅⋅(This is contrary to the typical GFM line break behaviour, where trailing spaces are not required.)</p>\n<ul>\n<li>Unordered list can use asterisks</li><li>Or minuses</li><li>Or pluses</li></ul>\n<p><a href="https://www.google.com">I&#39;m an inline-style link</a></p>\n<p><a href="https://www.google.com">I&#39;m an inline-style link with title</a></p>\n<p>[I&#39;m a reference-style link][Arbitrary case-insensitive reference text]</p>\n<p><a href="../blob/master/LICENSE">I&#39;m a relative reference to a repository file</a></p>\n<p>Here&#39;s our logo (hover to see the title text):</p>\n<p>Inline-style: </p>\n<p>Reference-style: \n<img src="https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png" alt="alt text"></p>\n\n        <div class="hljs javascript">\n          <pre>var s = <span class="hljs-string">"JavaScript syntax highlighting"</span><span class="hljs-comment">;</span>\nalert(s)<span class="hljs-comment">;</span></pre>\n        </div>\n      \n        <div class="hljs python">\n          <pre>s = <span class="hljs-string">"Python syntax highlighting"</span>\nprint s</pre>\n        </div>\n      \n        <div class="hljs">\n          <pre>No language indicated, so no syntax highlighting. \nBut let&#39;s throw in a &lt;b&gt;tag&lt;&#x2F;b&gt;.</pre>\n        </div>\n      ',
-      prev: 138196161,
-      next: 138194267
-    }
+    const post = posts[0]
+    const issue = issues[0]
 
     assert(posts.length === 5)
+    assert(post.id === issue.id)
+    assert(post.created === issue.created_at)
+    assert(post.updated === issue.updated_at)
+    assert(post.title === issue.title)
+    assert(`/${_config.post_dir}/${post.id}.html` === post.path)
+    assert(`/${_config.post_dir}/${post.id}.html` === post.url)
+    assert(post.author.name === issue.user.login)
+    assert(post.author.avatar === issue.user.avatar_url)
+    assert(post.author.url === issue.user.html_url)
+    assert(post.toc.trim() === '<ul><li><a href=\"#anh1header\"> An h1 header</a><ul><li><a href=\"#anh2header\"> An h2 header</a><ul><li><a href=\"#anh3header\"> An h3 header</a></li></ul></li></ul></li></ul>')
+    assert(post.category.id === issue.milestone.id)
+    assert(post.category.name === issue.milestone.title)
+    assert(posts[1].tags.length === issues[1].labels.length)
+    assert(posts[1].tags[0].id === issues[1].labels[0].id)
+    assert(posts[1].tags[0].name === issues[1].labels[0].name)
+    assert((/\.(gif|jpg|jpeg|png)$/i).test(post.thumb) === true)
+    assert(issue.body.indexOf(post.thumb) > -1)
+    assert(post.content.indexOf(post.thumb) === -1)
+    assert(post.summary === '')
+    assert(post.html === markeder(issue.body, true))
+    assert(posts[2].content === markeder(issues[2].body.replace(/<!--\s*more\s*-->/, '')))
+    assert(posts[2].summary === markeder(issues[2].body.split(/<!--\s*more\s*-->/)[0]))
 
-    assert(`/${_config.post_dir}/${posts[0].id}.html` === posts[0].url)
     assert(`/${_config.category_dir}/${posts[0].category.id}/` === posts[0].category.url)
     assert(`/${_config.category_dir}/0/` === posts[3].category.url)
     assert(_config.default_category === posts[3].category.name)
@@ -84,5 +93,11 @@ describe('processor', () => {
     assert(posts[0].prev === '')
     assert(posts[1].prev === posts[0].id)
     assert(posts[posts.length - 1].next === '')
+
+    _config.thumbnail_mode = 2
+    processor = new Processor(_config)
+    posts = (await processor.process(issues)).posts
+
+    assert(posts[0].content.indexOf(posts[0].thumb) > -1)
   })
 })
