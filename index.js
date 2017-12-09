f0608dcacddb90fa0e2d06af9994708c7acda3b9
LoeiFy <LoeiFy@gmail.com>
2017-12-09 16:28:00 +0800

    split post function

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-const Marked = require('acyort-marked')
 const Pagination = require('./lib/pagination')
-const postCategory = require('./lib/category')
-const postFilter = require('./lib/filter')
-const postPage = require('./lib/page')
-const postTag = require('./lib/tag')
-const postThumb = require('./lib/thumb')
-const postToc = require('./lib/toc')
-const postSummary = require('./lib/summary')
+const filterFn = require('./lib/filter')
+const pageFn = require('./lib/page')
+const postFn = require('./lib/post')
 
 class Processor extends Pagination {
-  constructor(config) {
-    const markeder = new Marked(config)
+  constructor({ config, markeder }) {
     super(config)
     this.config = config
     this.markeder = (...args) => markeder.mark(...args)
@@ -94,62 +88,24 @@ class Processor extends Pagination {
 
   setPosts(posts) {
     this.posts = posts.map((post, i) => {
-      const data = this.getPost(post)
+      const data = postFn(post, {
+        config: this.config,
+        markeder: this.markeder,
+      })
+
       data.prev = i > 0 ? posts[i - 1].id : ''
       data.next = i < posts.length - 1 ? posts[i + 1].id : ''
+
       return data
     })
   }
 
-  getPost(post) {
-    const { config, markeder } = this
-    const {
-      id,
-      created_at,
-      updated_at,
-      title,
-      user: {
-        login,
-        avatar_url,
-        html_url,
-      },
-      body,
-    } = post
-
-    const data = {
-      id,
-      created: created_at,
-      updated: updated_at,
-      title,
-      path: `/${config.post_dir}/${id}.html`,
-      url: `/${config.post_dir}/${id}.html`,
-      author: {
-        name: login,
-        avatar: avatar_url,
-        url: html_url,
-      },
-      html: markeder(body, true),
-      toc: postToc(body, markeder),
-      category: postCategory(post, config),
-      tags: postTag(post, config),
-    }
-
-    const { thumb, content } = postThumb(body, config)
-    const { summary, main } = postSummary(content)
-
-    data.thumb = thumb
-    data.summary = markeder(summary)
-    data.content = markeder(main)
-
-    return data
-  }
-
   setPages(pages) {
-    this.pages = pages.map(page => postPage(page, this.markeder))
+    this.pages = pages.map(page => pageFn(page, this.markeder))
   }
 
   process(issues) {
-    const { pages, posts } = postFilter(issues, this.config)
+    const { pages, posts } = filterFn(issues, this.config)
 
     if (!pages.length && !posts.length) {
       return Promise.reject(new Error('No content. Check user, repository or authors fields'))
